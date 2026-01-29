16c7e53268670ed1eb4bc0bd8c27ce6a12047db2
vb4370 <victoria.bigeagle@students.msswarriors.org>
2026-01-29 15:46:34 +0000

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country."
 
-let output = "quoteLength" + "twentySecondLetter";
+let output = "quoteLength" + [twentySecondLetter];
 
 
 document.getElementById('result').innerHTML = output;
