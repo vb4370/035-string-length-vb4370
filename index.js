31349119acde964b22272e61bb4126d89d4807a2
vb4370 <victoria.bigeagle@students.msswarriors.org>
2026-01-29 15:44:46 +0000

    idk

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-let quote = "...ask not what your country can do for you — ask what you can do for your country.";
+let quote = "...ask not what your country can do for you — ask what you can do for your country."
 
-//Add code below
 let output = "quoteLength" + "twentySecondLetter";
 
 
